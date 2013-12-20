7b5e09864952dc7376e09bd9e96d36238c3de0e5
dimitry <dimitrismistriotis@gmail.com>
2013-12-20 12:33:14 +0000

    added reference point to readme and ip display on mysql script

--- mysql/run_client.sh
+++ mysql/run_client.sh
@@ -5,5 +5,7 @@ TAG="mysql"
 CONTAINER_ID=$(docker ps | grep $TAG | awk '{print $1}')
  
 IP=$(docker inspect $CONTAINER_ID | python -c 'import json,sys;obj=json.load(sys.stdin);print obj[0]["NetworkSettings"]["IPAddress"]')
- 
+
+echo "IP: ${IP}"
+
 mysql -u admin -p -h $IP
